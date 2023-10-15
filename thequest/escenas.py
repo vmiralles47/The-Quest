65b928dba24601abbd5d8b84b4c0796697aeb420
Victor Miralles <victor_miralles@hotmail.com>
2023-10-15 11:22:15 +0200

    puesta en marcha del bucle de game.py con tres escenas ,vacías por ahora

--- thequest/escenas.py
+++ thequest/escenas.py
@@ -13,6 +13,9 @@ class Escena:
         """
         print("bucle principal de escena")
 
+    def barra_pulsada(self, evento):
+        if evento.type == pg.KEYDOWN and evento.key == pg.K_SPACE:
+            return True
 # pantalla inicial con título,la historia de THE QUEST y press space to continue/puede alternar con RECORDS
 
 
@@ -22,18 +25,74 @@ class Portada(Escena):
 
     def bucle_principal(self):
         super().bucle_principal()
-        pass
+        print("bucle principal de Portada")
+        salir = False
+        while not salir:
+            # 1 capturar los eventos
+            for evento in pg.event.get():
+                if evento.type == pg.QUIT:
+                    # ():
+                    return True
+                if self.barra_pulsada(evento):
+                    salir = True
+            # 2. Calcular estado de elementos y pintarlos elementos
+            self.pantalla.fill((99, 0, 0))
+            # pintar.logo
+            # pintar.backstory
+            # pintar "pulsar SPACE_BAR para empezar"
+
+            # 3. Mostrar los cambios (pintados) y controlar el reloj
+            pg.display.flip()
+        return False
 
 # pantalla donde se desarrolla cada nivel del juego. recibe como parámetro en qué nivel está para ajustar la dificultad
 
 
-class Juego(Escena, nivel=1):
+class Juego(Escena):
     def __init__(self, pantalla):
         super().__init__(pantalla)
-    pass
+
+    def bucle_principal(self):
+        super().bucle_principal()
+        print("bucle principal de Juego")
+        salir = False
+        while not salir:
+            # 1 capturar los eventos
+            for evento in pg.event.get():
+                if evento.type == pg.QUIT:
+                    # ():
+                    return True
+                if self.barra_pulsada(evento):
+                    salir = True
+            # 2. Calcular estado de elementos y pintarlos elementos
+            self.pantalla.fill((0, 99, 0))
+
+            # salir = self.empezar_partida()
+            # 3. Mostrar los cambios (pintados) y controlar el reloj
+            pg.display.flip()
+        return False
 
 
 class Records(Escena):
     def __init__(self, pantalla):
         super().__init__(pantalla)
-    pass
+
+    def bucle_principal(self):
+        super().bucle_principal()
+        print("bucle principal de Records")
+        salir = False
+        while not salir:
+            # 1 capturar los eventos
+            for evento in pg.event.get():
+                if evento.type == pg.QUIT:
+                    # ():
+                    return True
+                if self.barra_pulsada(evento):
+                    salir = True
+            # 2. Calcular estado de elementos y pintarlos elementos
+            self.pantalla.fill((0, 0, 99))
+
+            # salir = self.empezar_partida()
+            # 3. Mostrar los cambios (pintados) y controlar el reloj
+            pg.display.flip()
+        return False
--- thequest/game.py
+++ thequest/game.py
@@ -1,19 +1,24 @@
 import pygame as pg
 
 from . import ALTO, ANCHO
-from escenas import Nivel, Portada, Records
+from .escenas import Juego, Portada, Records
 
 
 class TheQuest:
     def __init__(self):
         pg.init()
-        self.pantalla = pg.display.set_mode(ANCHO, ALTO)
+        self.pantalla = pg.display.set_mode((ANCHO, ALTO))
         # importar las escenas necesarias para jugar: INICIO Y JUEGO (records puede ser llamada tanto desde Inicio como Juego)
         # pasarles como argumento self.pantalla y self.records
         portada = Portada(self.pantalla)
         juego = Juego(self.pantalla)
         records = Records(self.pantalla)
+        self.escenas = [portada, juego, records]
 
     def jugar(self):
-
-        pass
+        for escena in self.escenas:
+            he_acabado = escena.bucle_principal()
+            if he_acabado:
+                break
+        print("saliendo del bucle de thequest.jugar")
+        pg.quit()
